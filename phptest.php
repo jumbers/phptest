1088ec3704e74afb0fd148609ce3e467314f226c
jumbers <168681136+jumbers@users.noreply.github.com>
2024-05-01 16:35:11 -0400

    Update phptest.php

--- phptest.php
+++ phptest.php
@@ -1,26 +1,15 @@
-<?php
-echo 'h';
-if (!$viewdata) {exit(); } //don't actually bother to load the log if the user didn't yet make a selection
-if ($log == "") {exit ("Please specify a type of log"); }
-if ($search == "") {exit ("Please specify a search string"); }
-
-$cmdline = "whoami";
-
-exec($cmdline, $arrOutput, $errorOutput);
-#echo join("<br>", $arrOutput);
-if ($errorOutput != 0)
-	die("Error: $errorOutput<br>");
-
-
-// Format the output 
-
-$totallines = $arrOutput[0];
-#echo "I have $lines lines<br/>";
-array_shift($arrOutput);
-$output = join("\n", $arrOutput);
-$output = strip_tags($output);
-$output = str_replace("\n", "<br/>", $output);
+<html>
+ <head>
+  <title>PHP-Test</title>
+ </head>
+ <body>
+  <?php echo '<h1>Hello World!</h1><h3>Welcome to WPMU DEV</h3>'; 
+	$cmdline = "sudo -u wizbot /bin/bash /home/wizbot/searchspeciallog.sh $log $search";
 	
-echo "<p>" . $output . "</p>";
-	
-?> 
+	exec($cmdline, $arrOutput, $errorOutput);
+	if ($errorOutput != 0)
+		die("Error: $errorOutput<br>");
+	echo $arrOutput;
+	?>
+ </body>
+</html>
