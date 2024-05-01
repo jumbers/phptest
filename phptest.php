16469404496a2355edd29fe06e58c8c8920df957
jumbers <168681136+jumbers@users.noreply.github.com>
2024-05-01 16:30:53 -0400

    Update phptest.php

--- phptest.php
+++ phptest.php
@@ -1,7 +1,26 @@
-<?php 
-echo 'test';
-$cmdline2 = "whoami";
-exec($cmdline2, $arrOutput, $errorOutput);
-$cmdline3 = "sudo -u wizbot /bin/bash whoami";
-exec($cmdline3, $arrOutput, $errorOutput);
+<?php
+
+if (!$viewdata) {exit(); } //don't actually bother to load the log if the user didn't yet make a selection
+if ($log == "") {exit ("Please specify a type of log"); }
+if ($search == "") {exit ("Please specify a search string"); }
+
+$cmdline = "sudo -u wizbot whoami";
+
+exec($cmdline, $arrOutput, $errorOutput);
+#echo join("<br>", $arrOutput);
+if ($errorOutput != 0)
+	die("Error: $errorOutput<br>");
+
+
+// Format the output 
+
+$totallines = $arrOutput[0];
+#echo "I have $lines lines<br/>";
+array_shift($arrOutput);
+$output = join("\n", $arrOutput);
+$output = strip_tags($output);
+$output = str_replace("\n", "<br/>", $output);
+	
+echo "<p>" . $output . "</p>";
+	
 ?> 
