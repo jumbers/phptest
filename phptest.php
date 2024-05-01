bec507811b6a4ad1b661d33c35793128bf176531
jumbers <168681136+jumbers@users.noreply.github.com>
2024-05-01 17:03:01 -0400

    Update phptest.php

--- phptest.php
+++ phptest.php
@@ -3,8 +3,22 @@
   <title>PHP-Test</title>
  </head>
  <body>
-  <?php 
-	phpinfo();
+	<?php
+	$cmdline = "sudo -u wizbot /bin/bash whoami";
+	
+	exec($cmdline, $arrOutput, $errorOutput);
+
+	if ($errorOutput != 0)
+		die("Error: $errorOutput<br>");
+	$totallines = $arrOutput[0];
+	#echo "I have $lines lines<br/>";
+	array_shift($arrOutput);
+	$output = join("\n", $arrOutput);
+	$output = strip_tags($output);
+	$output = str_replace("\n", "<br/>", $output);
+		
+	echo "<p>" . $output . "</p>";
+		
 	?>
  </body>
 </html>
